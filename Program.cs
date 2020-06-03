759edb37a8161c7bb1a678ad7fa7033d48470b21
Bárbara <babyr.oliveira@gmail.com>
2020-06-02 23:47:12 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Humanizer; 
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Olá usuário, vou ajudá-lo a escrever qualquer número inteiro por extenso.");
+            Console.WriteLine(" Pressione enter para continuar:");
+            Console.ReadKey();
+
+            Console.WriteLine("Digite algum número:");
+            string number = (Console.ReadLine());
+            int number2 = Convert.ToInt32(number);
+            string nextenso = number2.ToWords();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{number} => {nextenso}");
+        }
+    }
+}
